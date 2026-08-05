73004770512328d5bf29fb3d85af886a8827e33c
JDWorld <satanicseries@gmail.com>
2026-08-05 05:45:34 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ if [ -f "$SRC_DIR/remove.sh" ]; then
     sudo chmod +x "/usr/share/jdwos-pkg/uninstall.d/jdwos-pm-remove.sh"
 fi
 
-clear && exec bash && sudo jdw-install jdwos-corelib
-clear && exec bash && sudo jdw-install jdwos-stm
+sudo jdw-install jdwos-corelib
+sudo jdw-install jdwos-stm
 
 echo "✔️  JDWorld OS: Gestore dei Pacchetti JDW installato e pronto all'azione!"
