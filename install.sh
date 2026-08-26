d795f4d6dbd412ec3a6fdf7d8c9f58646af2e5d0
JDWorld <satanicseries@gmail.com>
2026-08-26 07:55:49 +0200

    Aggiornamento install

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 # install.sh -  del Core Package Manager per JDWorld OS
+clear
+# Iniezione immediata della libreria condivisa centralizzata di Jack
+source "/usr/share/jdworldos/jdwos-lib/jdwos-hfcore.sh" 2>/dev/null
+source "/usr/share/jdworldos/jdwos-lib/jdwos-icore.sh" 2>/dev/null
+source "/usr/share/jdworldos/jdwos-lib/jdwos-rgb.sh" 2>/dev/null
+
 SRC_DIR=$1
 
 echo "📦 Installazione del JDWorld OS Package Manager Universale..."
- sudo pacman -S --needed --noconfirm kdialog
+# Blocco rivelamento PM e Installazione Dipendenze (pacman/apt)
+echo -e "${orange}[*] Sincronizzazione delle dipendenze di sistema su base $DISTROS...${reset}"
+# 1. RILEVAMENTO HARDWARE DEL PACKAGE MANAGER ATTIVO
+rivela_pm
+
+case "$PKGS" in
+    pacman)
+         sudo pacman -S --needed --noconfirm kdialog
+        ;;
+    apt)
+        # Sincronizzazione specchi Debian obbligatoria prima del push
+        sudo apt-get update -y && sudo apt-get install -y kdialog
+        ;;
+    *)
+        echo "Distribuzione non supportata"
+        echo "Visita https://JDWrld.github.io/jdworldos per maggiori info."
+        exit 1
+        ;;
+esac
+
+# 2. CONTINUAZIONE DEL DEPLOY COMUNE (Copia degli asset, permessi, etc.)
+echo -e "${green}[✓] Dipendenze verificate. Procedo con il trasferimento dei moduli...${reset}"
+# Fine Blocco rivelamento PM
+
 
 # 1. Crea le cartelle di archivio centralizzate per il database dei pacchetti
 sudo mkdir -p /usr/share/jdworldos/jdwos-pkg/installed
